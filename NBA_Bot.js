c298c4938472a585e0ee89b0702ab513cc58d07b
GurkarnSD <GurkarnSD@outlook.com>
2022-11-20 22:26:29 -0500

    Created a Discord Bot to access NBA Stats stored on MongoDB - NBA Stat Bot V1.0

---
+++ NBA_Bot.js
@@ -0,0 +1,243 @@
+const { Client, GatewayIntentBits, EmbedBuilder, ApplicationCommandPermissionType } = require('discord.js');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const mongo_client = new MongoClient(process.env.MONGO_KEY);
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+	console.log(commandName);
+	
+	if (commandName === 'teams') {
+		const teamname = interaction.options.getString('teamname');
+		const returnInfo = await TeamInfo(teamname);
+		const teamEmbed = new EmbedBuilder().setTitle(teamname).setColor(0xC70039).setThumbnail(returnInfo['team_logo'])
+		.addFields(
+			{ name: 'Start Year', value: returnInfo['team_startyear'], inline: true},
+			{ name: 'Lifespan', value: returnInfo['team_lifespan'], inline: true },
+			{ name: 'Games Played', value: returnInfo['team_games'], inline: true },
+			{ name: 'Wins', value: returnInfo['team_wins'], inline :true },
+			{ name: 'Losses', value: returnInfo['team_losses'], inline: true },
+			{ name: 'Win/Loss Percentage', value: returnInfo['team_wlp'], inline: true },
+			{ name: 'Playoff Apperances', value: returnInfo['team_plyfs'], inline: true },
+			{ name: 'Division Champions', value: returnInfo['team_divchamp'], inline: true },
+			{ name: 'Conference Champions', value: returnInfo['team_confchamp'], inline: true },
+			{ name: 'NBA Champions', value: returnInfo['team_nbachamp'], inline: true }
+		);
+		await interaction.reply(
+			{ embeds : [teamEmbed]}
+			);
+	}
+
+	if (commandName === 'player') {
+		const playername = interaction.options.getString('player');
+		const playerInfo = await Player(playername);
+		const playerEmbed = new EmbedBuilder().setTitle(playername).setColor(0xC70039).setThumbnail(playerInfo['player_img'])
+		.addFields(
+			{ name: 'Position', value: playerInfo['player_pos'] },
+			{ name: 'Height', value: playerInfo['player_height'] },
+			{ name: 'Weight', value: playerInfo['player_weight'] },
+			{ name: 'Years In NBA', value: playerInfo['player_yearsexp'] },
+			{ name: 'College', value: playerInfo['player_college']}
+		);
+		await interaction.reply(
+			{ embeds : [playerEmbed]}
+			);
+	}
+
+	if (commandName === 'roster') {
+		const teamname = interaction.options.getString('team');
+		const roster = await TeamRoster(teamname);
+		var rosterString = ''
+		roster.forEach((player) => {
+			rosterString += player + '\n'
+		});
+		const rosterEmbed = new EmbedBuilder().setTitle(teamname).setColor(0xC70039)
+		.addFields(
+			{ name: 'Roster', value: rosterString}
+		);
+		await interaction.reply(
+			{ embeds: [rosterEmbed]}
+		);
+	}
+
+	if (commandName === 'teamseason') {
+		const teamname = interaction.options.getString('team');
+		const season = interaction.options.getString('season');
+		var year = parseInt(season);
+		var seasonyear = year.toString() +'-'+ (year + 1).toString().slice(2);
+		const teamseason = await TeamSeason(teamname, seasonyear);
+		var wins = parseInt(teamseason['season_wins']);
+		var losses = parseInt(teamseason['season_losses']);
+		var gp = wins + losses;
+		const seasonEmbed = new EmbedBuilder().setTitle(teamname).setColor(0xC70039)
+		.addFields(
+			{ name: 'Season', value: teamseason['season_year'] },
+			{ name: 'Wins', value: teamseason['season_wins'] },
+			{ name: 'Losses', value: teamseason['season_losses'] },
+			{ name: 'Games Played', value: gp.toString() },
+			{ name: 'W/L%', value: teamseason['season_wlp'] },
+			{ name: 'Coach', value: teamseason['season_coach'] }
+		);
+			
+		await interaction.reply(
+			{ embeds: [seasonEmbed]}
+			);
+	}
+
+	if (commandName === 'playerseason') {
+		const playername = interaction.options.getString('player');
+		const playerInfo = await Player(playername);
+		const season = interaction.options.getString('season');
+		var year = parseInt(season);
+		var seasonYear = year.toString() +'-'+ (year + 1).toString().slice(2);
+		const plSeason = playerSeason(playerInfo, seasonYear);
+
+		const pSeasonEmbed = new EmbedBuilder().setTitle(playername).setColor(0xC70039)
+		.addFields(
+			{ name: 'Age', value: plSeason['stat_age'], inline: true },
+			{ name: 'Position', value: plSeason['stat_pos'], inline: true },
+			{ name: 'Games Played', value: plSeason['stat_g'], inline: true },
+			{ name: 'Games Started', value: plSeason['stat_gs'], inline: true },
+			{ name: 'Minutes', value: plSeason['stat_mp'], inline: true },
+			{ name: 'FG %', value: plSeason['stat_fg'], inline: true },
+			{ name: 'FG3 %', value: plSeason['stat_fg3'], inline: true },
+			{ name: 'FG2 %', value: plSeason['stat_fg2'], inline: true },
+			{ name: 'FT %', value: plSeason['stat_ftpct'], inline: true },
+			{ name: 'Assists', value: plSeason['stat_ast'], inline: true },
+			{ name: 'Rebounds', value: plSeason['stat_trb'], inline: true },
+			{ name: 'Steals', value: plSeason['stat_stl'], inline: true },
+			{ name: 'Points', value: plSeason['stat_pts'], inline: true },
+		);
+		await interaction.reply(
+			{ embeds: [pSeasonEmbed]}
+		);
+	}
+
+	if (commandName === 'playerplayoff') {
+		const playername = interaction.options.getString('player');
+		const playerInfo = await Player(playername);
+		const playoff = interaction.options.getString('playoff');
+		var year = parseInt(playoff);
+		var playoffYear = year.toString() +'-'+ (year + 1).toString().slice(2);
+		const plPlayoff = playerPlayoff(playerInfo, playoffYear);
+		const pPlayoffEmbed = new EmbedBuilder().setTitle(playername).setColor(0xC70039)
+		.addFields(
+			{ name: 'Age', value: plPlayoff['stat_age'], inline: true },
+			{ name: 'Position', value: plPlayoff['stat_pos'], inline: true },
+			{ name: 'Games Played', value: plPlayoff['stat_g'], inline: true },
+			{ name: 'Games Started', value: plPlayoff['stat_gs'], inline: true },
+			{ name: 'Minutes', value: plPlayoff['stat_mp'], inline: true },
+			{ name: 'FG %', value: plPlayoff['stat_fg'], inline: true },
+			{ name: 'FG3 %', value: plPlayoff['stat_fg3'], inline: true },
+			{ name: 'FG2 %', value: plPlayoff['stat_fg2'], inline: true },
+			{ name: 'FT %', value: plPlayoff['stat_ftpct'], inline: true },
+			{ name: 'Assists', value: plPlayoff['stat_ast'], inline: true },
+			{ name: 'Rebounds', value: plPlayoff['stat_trb'], inline: true },
+			{ name: 'Steals', value: plPlayoff['stat_stl'], inline: true },
+			{ name: 'Points', value: plPlayoff['stat_pts'], inline: true },
+		);
+		await interaction.reply(
+			{ embeds: [pPlayoffEmbed]}
+		);
+	}
+})
+
+client.login(process.env.DISCORD_TOKEN);
+
+async function TeamInfo(teamname) {
+    try {
+        await mongo_client.connect();
+        const TeamInfo = mongo_client.db('TeamInfo');
+        var Team = TeamInfo.collection(teamname).find();
+        var data = await Team.toArray();
+		var dict = {}
+        data.forEach((result) => {
+            dict = result
+        })
+		return dict;
+
+    } catch {
+		console.log("error")
+	} finally {
+        await mongo_client.close();
+    }
+}
+
+async function TeamRoster(teamname) {
+	try {
+        await mongo_client.connect();
+        const Team = mongo_client.db(teamname.replace(' ', ''));
+        var Players = await Team.collection('Players').find().toArray();
+		var Roster = []
+        Players.forEach((player) => {
+			Roster.push(player['player_name'])
+		})
+		return Roster;
+    } catch {
+		console.log("error")
+	} finally {
+        await mongo_client.close();
+    }
+}
+
+async function Player(playername) {
+	try {
+        await mongo_client.connect();
+        const Teams = await mongo_client.db('TeamInfo').listCollections().toArray();
+		var returnPlayer;
+		await Promise.all(Teams.map(async (team) => {
+			var Player = await mongo_client.db(team['name'].replaceAll(' ', '')).collection('Players').find({ player_name: playername}).toArray();
+			
+			if ( typeof Player[0] !== 'undefined'){
+				returnPlayer = Player[0];
+				return;
+			}
+		  }));
+
+		return returnPlayer;
+
+    } catch (error) {
+		console.log(error)
+	} finally {
+        await mongo_client.close();
+    }
+}
+
+async function TeamSeason(teamname, seasonyear) {
+	try {
+        await mongo_client.connect();
+        const Team = mongo_client.db(teamname.replace(' ', ''));
+        var Seasons = await Team.collection('Seasons').find().toArray();
+		var returnSeason;
+		Seasons.forEach((season) => {
+			if (season['season_year'] === seasonyear) {
+				returnSeason = season;
+			}
+		})
+		return returnSeason;
+
+    } catch {
+		console.log("error")
+	} finally {
+        await mongo_client.close();
+    }
+}
+
+function playerSeason(playerInfo, seasonYear) {
+	var seasons = playerInfo['Seasons'];
+	var season = seasons[seasonYear];
+	return season;
+}
+
+function playerPlayoff(playerInfo, playoffYear) {
+	var playoffs = playerInfo['Playoffs'];
+	var playoff = playoffs[playoffYear];
+	return playoff;
+}
---
+++ bot-commands.js
@@ -0,0 +1,55 @@
+const { REST, SlashCommandBuilder, Routes } = require('discord.js');
+require('dotenv').config();
+
+const commands = [
+    new SlashCommandBuilder().setName('teams').setDescription('Replies with Teams')
+        .addStringOption(option =>
+            option.setName('team')
+            .setDescription('Return Info About The Selected Team')
+            .setRequired(true)),
+    new SlashCommandBuilder().setName('player').setDescription('Returns a players stats')
+        .addStringOption(option => 
+            option.setName('player')
+            .setDescription('Return Info About The Selected Player')
+            .setRequired(true)),
+    new SlashCommandBuilder().setName('roster').setDescription('Returns a teams current roster')
+        .addStringOption(option => 
+            option.setName('team')
+            .setDescription('Return Roster of The Selected Team')
+            .setRequired(true)),
+    new SlashCommandBuilder().setName('teamseason').setDescription('Returns the stats of a team during the specified season')
+        .addStringOption(option => 
+            option.setName('team')
+            .setDescription('Return Info About The Selected Team')
+            .setRequired(true))
+        .addStringOption(option => 
+            option.setName('season')
+            .setDescription('Return Info About The Selected Season')
+            .setRequired(true)),
+    new SlashCommandBuilder().setName('playerseason').setDescription('Returns the stats of a player during the specified season')
+        .addStringOption(option => 
+            option.setName('player')
+            .setDescription('Return Info About The Selected Player')
+            .setRequired(true))
+        .addStringOption(option => 
+            option.setName('season')
+            .setDescription('Return Info About The Selected Season')
+            .setRequired(true)),
+    new SlashCommandBuilder().setName('playerplayoff').setDescription('Returns the stats of a player during the specified playoff')
+        .addStringOption(option => 
+            option.setName('player')
+            .setDescription('Return Info About The Selected Player')
+            .setRequired(true))
+        .addStringOption(option => 
+            option.setName('playoff')
+            .setDescription('Return Info About The Selected Playoff')
+            .setRequired(true))
+]
+
+    .map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+rest.put(Routes.applicationGuildCommands(process.env.clientId, process.env.guildId), { body: commands })
+    .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+    .catch(console.error)
